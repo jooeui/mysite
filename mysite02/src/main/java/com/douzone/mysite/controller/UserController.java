3733a94eb6ff46f3d3a93c6fa876616193e105c4
jooeui <kje_0727@naver.com>
2021-10-12 13:50:50 +0900

    21-10-12 Action & ActionFactory

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.web.util.MvcUtils;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -14,14 +16,20 @@ public class UserController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		
-		String action = request.getParameter("a");
+		String actionName = request.getParameter("a");
 		
-		if("joinform".equals(action)) {
-			MvcUtils.forward("/WEB-INF/views/user/joinform.jsp", request, response);
-		} else {
-			// MvcUtils.redirect(request.getContextPath(), request,response);
-			MvcUtils.redirect("/mysite02", request,response);
-		}
+		ActionFactory af = new UserActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute(request, response);
+		
+		
+		
+//		if("joinform".equals(action)) {
+//			MvcUtils.forward("/WEB-INF/views/user/joinform.jsp", request, response);
+//		} else {
+//			// MvcUtils.redirect(request.getContextPath(), request,response);
+//			MvcUtils.redirect("/mysite02", request,response);
+//		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ mysite02/src/main/java/com/douzone/mysite/mvc/UserActionFactory.java
@@ -0,0 +1,19 @@
+package com.douzone.mysite.mvc;
+
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
+
+public class UserActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		Action action = null;
+		if("joinform".equals(actionName)) {
+			// action = new JoinFormAction();
+		} else {
+		}
+		
+		return action;
+	}
+
+}
