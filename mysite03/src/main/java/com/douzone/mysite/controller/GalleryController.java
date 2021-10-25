8212de94a44605af7d7a636b02304f2a4728bf8b
jooeui <kje_0727@naver.com>
2021-10-25 23:07:04 +0900

    mysite03 gallery 추가

--- mysite03/src/main/java/com/douzone/mysite/controller/GalleryController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GalleryController.java
@@ -1,14 +1,46 @@
 package com.douzone.mysite.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.douzone.mysite.service.GalleryService;
+import com.douzone.mysite.vo.GalleryVo;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
+	@Autowired
+	private GalleryService galleryService;
 	
 	@RequestMapping("")
-	public String index() {
+	public String index(Model model) {
+		List<GalleryVo> list = galleryService.getImages();
+//		System.out.println(list);
+		model.addAttribute("list", list);
 		return "gallery/index";
+	}
+	
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
+	public String upload(
+			@RequestParam(value="file") MultipartFile multipartFile,
+			@RequestParam(value="comments", required=true, defaultValue="") String comments,
+			Model model) {
+		galleryService.uploadImage(multipartFile, comments);
+		return "redirect:/gallery";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
+	public String delete(
+			@PathVariable(value="no") Long no) {
+		galleryService.deleteImage(no);
+		return "redirect:/gallery";
 	}
 }
